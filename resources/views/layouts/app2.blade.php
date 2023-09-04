d58ffe46d54d1b9a417561a919f215d385d0ead5
PRIMAINSANI <anwar.fauzy18@gmail.com>
2023-09-04 16:00:20 +0700

    Perbaikan Template

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -9,9 +9,9 @@
     <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
-    <title>Dashboard ecommerce - Vuexy - Bootstrap HTML admin template</title>
-    <link rel="apple-touch-icon" href="{{asset('template/app-assets/images/ico/apple-icon-120.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('template/app-assets/images/ico/favicon.ico')}}">
+    @yield('title')
+    <link rel="apple-touch-icon" href="{{asset('storage/icon.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('storage/icon.png')}}">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
@@ -46,7 +46,8 @@
 <body class="vertical-layout vertical-menu-modern  navbar-floating footer-static   menu-collapsed" data-open="click" data-menu="vertical-menu-modern" data-col="">
     @include('layouts.navbar')
     @include('layouts.sidebar')
-    @include('layouts.content')
+    @yield('content')'cdvfghjkl;'
+    
     @include('layouts.footer')
     <!-- BEGIN: Vendor JS-->
     <script src="{{asset('template/app-assets/vendors/js/vendors.min.js')}}"></script>
